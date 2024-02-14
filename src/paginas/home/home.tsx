69ad8bbdfef55d6b1192cd717f6687484a4e7d6e
David Bomfim <davidrochabomfim6@gmail.com>
2024-02-14 15:37:52 -0300

    First Commit

---
+++ src/paginas/home/home.tsx
@@ -0,0 +1,16 @@
+import "./home.css"
+
+
+function Home() {
+    return (
+        <>
+            <h1 className="titulo">Miyamoto Musashi: O Maior Samurai da História</h1>
+            <img className="img" src="https://imgs.search.brave.com/8MnXOnedstuEYeNIjVwCzRdPUHzFOXbm5xqK7EHE_iI/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jNC53/YWxscGFwZXJmbGFy/ZS5jb20vd2FsbHBh/cGVyLzcvNzM1Lzc1/Ny9taXlhbW90by1t/dXNhc2hpLXNhbXVy/YWktd2FsbHBhcGVy/LXByZXZpZXcuanBn" alt=" Foto do Miyamoto Musashi" />
+
+
+
+        </>
+    )
+
+}
+export default Home;
